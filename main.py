73f3981ca45e2476a4b264a1385fad34a2f6cf90
shessantisocial <awojideglory@gmail.com>
2024-04-15 10:28:03 +0100

    first commit

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print(1)
-print("Hello")
+print("What is your first name?")
+fname = str(input())
+
+print("What is your last name?")
+lname = str(input())
+
+print("Input your phone number:")
+contact = int(input())
+
+name = str(input)
+print(f"Hello, {fname} {lname} your account has been created.")
+
